6128f29978d0fde096687669304cc0174f48e384
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-17 10:56:07 -0600

    Final Commit

--- reward_points_app/src/App.js
+++ reward_points_app/src/App.js
@@ -1,116 +1,47 @@
-// A retailer offers a rewards program to its customers, awarding points based on each recorded purchase.
-// A customer receives 2 points for every dollar spent over $100 in each transaction,
-// plus 1 point for every dollar spent over $50 in each transaction
-// (e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).
-// Given a record of every transaction during a three month period,
-// calculate the reward points earned for each customer per month and total.
-
-/*
-
-function calculateRewards(price) {
-  if (price >= 50 && price < 100) {
-    return price - 50;
-  } else if (price > 100) {
-    return 2 * (price - 100) + 50;
-  }
-  return 0;
-}
-
-class Transaction {
-  constructor(price) {
-    this.price = price;
-    this.rewards = calculateRewards(price);
-    this.transactionDate = new Date();
-  }
-}
-
-class TransactionList {
-  constructor() {
-    this.list = [];
-  }
-
-  getLast3MonthsList() {
-    var today = new Date();
-    const threeOldDate = today.setMonth(today.getMonth() - 3);
-    let filteredList = this.list.filter(
-      (trans) => trans.transactionDate > threeOldDate
-    );
-    return filteredList.sort((a, b) => b.transactionDate - a.transactionDate);
-  }
-
-  getAllTransactions() {
-    return this.list.sort((a, b) => b.transactionDate - a.transactionDate);
-  }
-
-  addTransaction(price) {
-    const transaction = new Transaction(price);
-    this.list.push(transaction);
-  }
-
-  getTotalRewards() {
-    return this.list.length
-      ? this.list.reduce((acc, key) => key.rewards + acc, 0)
-      : 0;
-  }
-
-  rewardPerMonth() {
-    let last3MonthRewardsInDesc = [];
-    for (let i = 0; i < 3; i++) {
-      let filteredList = this.list.filter(
-        (trans) =>
-          trans.transactionDate.getMonth() === new Date().getMonth() - i
-      );
-      last3MonthRewardsInDesc[i] = filteredList.reduce(
-        (acc, key) => key.rewards + acc,
-        0
-      );
-    }
-    return last3MonthRewardsInDesc;
-  }
-}
-
-let myTransactionList = new TransactionList();
-myTransactionList.addTransaction(154);
-myTransactionList.addTransaction(54);
-myTransactionList.addTransaction(200);
-myTransactionList.addTransaction(20);
-myTransactionList.addTransaction(300);
-let arr = myTransactionList.getAllTransactions();
-
-
-*/
-/////////////////////////////////
-
-
 import React, { useState, useEffect } from "react";
 import "./App.css";
-// will use react-table npm package
-// it would be easy to use lodash or any kind od npm package
+import fetch from "./api/dataService";
+import ReactTable from "react-table";
+import _ from "lodash";
 
-function calculatePoints(inputData) { 
-
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  const pointsPerTransaction = inputData.map(transaction=> {
+function calculatePoints(inputData) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const pointsPerTransaction = inputData.map((transaction) => {
     let points = 0;
     let greaterThan100 = transaction.amt - 100;
-    
-    if (greaterThan100 > 0) {  // As per the business logic a customer receives 2 points for every dollar spent greater than $100 in each transaction
-      points += (greaterThan100 * 2);
-    }    
-    if (transaction.amt > 50) { // As per the business logic a customer receives 1 point for every dollar spent greater than $50 in each transaction
-      points += 50;      
+
+    if (greaterThan100 > 0) {
+      // As per the business logic a customer receives 2 points for every dollar spent greater than $100 in each transaction
+      points += greaterThan100 * 2;
+    }
+    if (transaction.amt > 50) {
+      // As per the business logic a customer receives 1 point for every dollar spent greater than $50 in each transaction
+      points += 50;
     }
     const month = new Date(transaction.transactionDt).getMonth();
-    return {...transaction, points, month};
+    return { ...transaction, points, month };
   });
-               
+
   let byCustomer = {};
   let totalPointsByCustomer = {};
-  pointsPerTransaction.forEach(pointsPerTransaction => {
-    let {custid, name, month, points} = pointsPerTransaction;   
+  pointsPerTransaction.forEach((pointsPerTransaction) => {
+    let { custid, name, month, points } = pointsPerTransaction;
     if (!byCustomer[custid]) {
-      byCustomer[custid] = [];      
-    }    
+      byCustomer[custid] = [];
+    }
     if (!totalPointsByCustomer[custid]) {
       totalPointsByCustomer[name] = 0;
     }
@@ -118,65 +49,150 @@ function calculatePoints(inputData) {
     if (byCustomer[custid][month]) {
       byCustomer[custid][month].points += points;
       byCustomer[custid][month].monthNumber = month;
-      byCustomer[custid][month].numTransactions++;      
-    }
-    else {
-      
+      byCustomer[custid][month].numTransactions++;
+    } else {
       byCustomer[custid][month] = {
         custid,
         name,
-        monthNumber:month,
+        monthNumber: month,
         month: months[month],
-        numTransactions: 1,        
-        points
-      }
-    }    
+        numTransactions: 1,
+        points,
+      };
+    }
   });
+  let tot = [];
+  for (var custKey in byCustomer) {
+    byCustomer[custKey].forEach((cRow) => {
+      tot.push(cRow);
+    });
+  }
+  //console.log("byCustomer", byCustomer);
+  //console.log("tot", tot);
+  let totByCustomer = [];
+  for (custKey in totalPointsByCustomer) {
+    totByCustomer.push({
+      name: custKey,
+      points: totalPointsByCustomer[custKey],
+    });
+  }
+  return {
+    summaryByCustomer: tot,
+    pointsPerTransaction,
+    totalPointsByCustomer: totByCustomer,
+  };
+}
 
-  function App() {
+function App() {
   const [transactionData, setTransactionData] = useState(null);
-  
-  const tableCol = [];
-  const tableRow = [];
 
-  if (transactionData == null) {
-    return <div>Loading...</div>;   
+  const columns = [
+    {
+      Header: "Customer",
+      accessor: "name",
+    },
+    {
+      Header: "Month",
+      accessor: "month",
+    },
+    {
+      Header: "# of Transactions",
+      accessor: "numTransactions",
+    },
+    {
+      Header: "Reward Points",
+      accessor: "points",
+    },
+  ];
+  const totalsByColumns = [
+    {
+      Header: "Customer",
+      accessor: "name",
+    },
+    {
+      Header: "Points",
+      accessor: "points",
+    },
+  ];
+
+  function getIndividualTransactions(row) {
+    let byCustMonth = _.filter(transactionData.pointsPerTransaction, (tRow) => {
+      return (
+        row.original.custid === tRow.custid &&
+        row.original.monthNumber === tRow.month
+      );
+    });
+    return byCustMonth;
   }
 
-  return transactionData == null ?
-    <div>Loading...</div> 
-      :    
-    <div>      
-      
+  useEffect(() => {
+    fetch().then((data) => {
+      const results = calculatePoints(data);
+      setTransactionData(results);
+    });
+  }, []);
+  if (transactionData == null) {
+    return <div>Loading...</div>;
+  }
+
+  return transactionData == null ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
       <div className="container">
         <div className="row">
           <div className="col-10">
             <h2>Points Rewards System Totals by Customer Months</h2>
           </div>
         </div>
-        <div>
-          <div>
+        <div className="row">
+          <div className="col-8">
+            <ReactTable
+              data={transactionData.summaryByCustomer}
+              defaultPageSize={5}
+              columns={columns}
+              SubComponent={(row) => {
                 return (
                   <div>
-                      {getIndividualTransactions(row).map(transaction=>{
-                        return <div className="container">
-                          <div>
-                            <div>
-                              <strong>Transaction Date:</strong> {transaction.transactionDt} - <strong>$</strong>{transaction.amt} - <strong>Points: </strong>{transaction.points}
+                    {getIndividualTransactions(row).map((tran) => {
+                      return (
+                        <div className="container">
+                          <div className="row">
+                            <div className="col-8">
+                              <strong>Transaction Date:</strong>{" "}
+                              {tran.transactionDt} - <strong>$</strong>
+                              {tran.amt} - <strong>Points: </strong>
+                              {tran.points}
                             </div>
                           </div>
                         </div>
-                      })}                                    
+                      );
+                    })}
                   </div>
-                )
-                          
-            </div>
+                );
+              }}
+            />
           </div>
         </div>
-        
-            
-    </div>
-  ;
-}
+      </div>
 
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <h2>Points Rewards System Totals By Customer</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <ReactTable
+              data={transactionData.totalPointsByCustomer}
+              columns={totalsByColumns}
+              defaultPageSize={5}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
---
+++ reward_points_app/src/api/dataService.js
@@ -0,0 +1,101 @@
+export default function fetch() {
+  // simulates data coming from a database.
+  return Promise.resolve([
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 120,
+      transactionDt: "05-01-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 75,
+      transactionDt: "05-21-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 94,
+      transactionDt: "05-21-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 10,
+      transactionDt: "06-01-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 75,
+      transactionDt: "06-21-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 200,
+      transactionDt: "07-01-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 1,
+      transactionDt: "07-04-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 80,
+      transactionDt: "07-03-2021",
+    },
+    {
+      custid: 1,
+      name: "Customer1",
+      amt: 224,
+      transactionDt: "07-21-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 125,
+      transactionDt: "05-01-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 75,
+      transactionDt: "05-21-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 10,
+      transactionDt: "06-01-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 75,
+      transactionDt: "06-21-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 200,
+      transactionDt: "07-01-2021",
+    },
+    {
+      custid: 2,
+      name: "Customer2",
+      amt: 224,
+      transactionDt: "07-21-2021",
+    },
+    {
+      custid: 3,
+      name: "Customer3",
+      amt: 120,
+      transactionDt: "06-21-2021",
+    },
+  ]);
+}
